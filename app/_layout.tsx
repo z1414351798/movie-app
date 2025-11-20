201b3c34dac0aa8632b07a06470753b106a66fe3
Andrea <Andrea.Chen@verystar.cn>
2025-11-20 10:38:33 +0800

    add google ads

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,9 +2,25 @@ import { Stack } from "expo-router";
 import "./globals.css";
 import { StatusBar } from "react-native";
 import { AuthProvider } from "@/context/AuthContext";
+import { AdMobInterstitial } from "expo-ads-admob";
+import { useEffect } from "react";
 
 
 export default function RootLayout() {
+  useEffect(() => {
+    const showAd = async () => {
+      try {
+        await AdMobInterstitial.setAdUnitID('ca-app-pub-3940256099942544/1033173712'); // test ID
+        await AdMobInterstitial.requestAdAsync({ servePersonalizedAds: true });
+        await AdMobInterstitial.showAdAsync();
+      } catch (error) {
+        console.log('Interstitial ad error:', error);
+      }
+    };
+
+    showAd();
+  }, []);
+
   return (
     <>
       <StatusBar hidden={true} />
